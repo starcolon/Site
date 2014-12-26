7cf54864627ca31342a2bf42a6d2b125c4ca3121
starcolon <pataoengineer@gmail.com>
2014-12-26 12:11:40 +0700

    FIRST COMMIT

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+/*========================================
+/*	StarColon Site 2014
+/*	Main js
+/*
+/*	by StarColon Projects
+/*======================================*/
+
+(function init(){
+
+	alert('hey');
+
+	// TAOTODO:Initialize the parallax scroller toolkit
+
+})();
