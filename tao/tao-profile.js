005d54354ba12b5ad16152668dfce7a49a43e226
starcolon <tao@starcolon.com>
2016-05-21 14:12:38 +0700

    Tao: Draw skill charts

---
+++ tao/tao-profile.js
@@ -0,0 +1,20 @@
+var profile = {
+  languages: {
+    labels: ["C++","JS","C#","Python","Ruby","Scala","Haskell"],
+    datasets: [
+      {
+        label: "",
+        data:[70,100,60,85,50,45,60]    
+      }
+    ]
+  },
+  dynamic: {
+    labels: ["Machine Learning","Computer Vision","Image Processing","NLP","Backend","Frontend"],
+    datasets: [
+      {
+        label: "",
+        data:[70,50,60,50,70,40]
+      }
+    ]
+  }
+}
